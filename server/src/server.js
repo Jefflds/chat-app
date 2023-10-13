2bb6eaba16f1d170fa91b737fd962d9552352a94
Jefferson Laurindo da Silva <107891837+Jefflds@users.noreply.github.com>
2023-10-12 21:36:32 -0300

    Configuracao da variavel de ambiente

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,13 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 
 const index = require('./routes/index');
 
-const IP = '0.0.0.0'; 
+const IP = process.env.IP || '0.0.0.0'; 
 const PORT = process.env.PORT || 3000;
+
 
 app.use("/", index);
 
