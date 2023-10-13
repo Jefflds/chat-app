fc48105d347237dec7fb30b2721a19eb4b616e06
Jefferson Laurindo da Silva <107891837+Jefflds@users.noreply.github.com>
2023-10-12 21:33:41 -0300

    Cricao das pastas e configuracao basica do server

---
+++ server/src/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+const index = require('./routes/index');
+
+const IP = '0.0.0.0'; 
+const PORT = process.env.PORT || 3000;
+
+app.use("/", index);
+
+app.listen(PORT, IP, () => {
+  console.log(`Servidor está rodando em http://${IP}:${PORT}`);
+});
